39d612b1e2a28a6bdf73d988f320e2e1587c6f36
Aleph90 <108907830+Aleph90@users.noreply.github.com>
2023-12-15 13:57:51 -0500

    Edited colour conversions
    Defined callable classes PieceWiseGaussian, XYZColorMatching, XYZtoRGBConverter.

--- color_conversion.py
+++ color_conversion.py
@@ -4,71 +4,62 @@ from math import exp
 from tuples import RGBColor
 
 
-def piece_wise_gaussian(x, alpha, mu, sigma_1, sigma_2):
-    if 10*x < mu:
-        sigma = sigma_1
-    else:
-        sigma = sigma_2
-    return alpha * exp(-((10*x-mu)/sigma)**2 / 2)
+class PieceWiseGaussian:
+    def __init__(self, alpha: float, mu: int, sigma_1: int, sigma_2: int):
+        self.alpha = alpha
+        self.mu = mu
+        self.sigma_1 = sigma_1
+        self.sigma_2 = sigma_2
+
+    def __call__(self, x: float) -> float:
+        sigma = self.sigma_1 if 10*x < self.mu else self.sigma_2
+        return self.alpha * exp(-((10*x - self.mu)/sigma)**2 / 2)
 
 
-# XYZ colour matching functions
+class XYZColorMatching:
+    def __init__(self, components: list[PieceWiseGaussian]):
+        self.components = components
 
-def x_bar(wavelength):
-    function_output = piece_wise_gaussian(wavelength, 1.056, 5998, 379, 310) + \
-        piece_wise_gaussian(wavelength, 0.362, 4420, 160, 267) + \
-        piece_wise_gaussian(wavelength, -0.065, 5011, 204, 262)
-    # print(function_output)
-    return function_output
+    def __call__(self, x: float) -> float:
+        return sum([component(x) for component in self.components])
 
 
-def y_bar(wavelength):
-    function_output = piece_wise_gaussian(wavelength, 0.821, 5688, 469, 405) + \
-        piece_wise_gaussian(wavelength, 0.286, 5309, 163, 311)
-    # print(function_output)
-    return function_output
+class XYZtoRGBConverter:
+    def __init__(self, c_x: float, c_y: float, c_z: float):
+        self.c_x = c_x
+        self.c_y = c_y
+        self.c_z = c_z
+
+    def __call__(self, x: float, y: float, z: float) -> float:
+        return self.c_x * x + self.c_y * y + self.c_z * z
 
 
-def z_bar(wavelength):
-    function_output = piece_wise_gaussian(wavelength, 1.217, 4370, 118, 360) + \
-        piece_wise_gaussian(wavelength, 0.681, 4590, 260, 138)
-    # print(function_output)
-    return function_output
+# XYZ color matching function
+x_bar = XYZColorMatching([PieceWiseGaussian(1.056, 5998, 379, 310),
+                         PieceWiseGaussian(0.362, 4420, 160, 267),
+                         PieceWiseGaussian(-0.065, 5011, 204, 262)])
+y_bar = XYZColorMatching([PieceWiseGaussian(0.821, 5688, 469, 405),
+                          PieceWiseGaussian(0.286, 5309, 163, 311)])
+z_bar = XYZColorMatching([PieceWiseGaussian(1.217, 4370, 118, 360),
+                          PieceWiseGaussian(0.681, 4590, 260, 138)])
 
 
-# RGB colour matching functions
-
-def r_bar(wavelength):
-    function_output = 3.2404542 * x_bar(wavelength) - 1.5371385 * y_bar(wavelength) - 0.4985314 * z_bar(wavelength)
-    # print(function_output)
-    return function_output
-
-
-def g_bar(wavelength):
-    function_output = - 0.9692660 * x_bar(wavelength) + 1.8760108 * y_bar(wavelength) + 0.0415560 * z_bar(wavelength)
-    # print(function_output)
-    return function_output
-
-
-def b_bar(wavelength):
-    function_output = 0.0556434 * x_bar(wavelength) - 0.2040259 * y_bar(wavelength) + 1.0572252 * z_bar(wavelength)
-    # print(function_output)
-    return function_output
+# RGB color matching functions
+r_bar = XYZtoRGBConverter(3.2404542, -1.5371385, -0.4985314)
+g_bar = XYZtoRGBConverter(-0.9692660, 1.8760108, 0.0415560)
+b_bar = XYZtoRGBConverter(0.0556434, -0.2040259, 1.0572252)
 
 
 # Converter
-
 def truncation(intensity, minimum=0, maximum=1):
     return min(max(intensity, minimum), maximum)
 
 
-def wavelength_to_rgb(wavelength, gamma=1):
-    r = (truncation(r_bar(wavelength)))**gamma
-    g = (truncation(g_bar(wavelength)))**gamma
-    b = (truncation(b_bar(wavelength)))**gamma
+def wavelength_to_rgb(wl: float, gamma=1) -> RGBColor:
+    x = x_bar(wl)
+    y = x_bar(wl)
+    z = z_bar(wl)
+    r = truncation(r_bar(x, y, z))**gamma
+    g = truncation(g_bar(x, y, z))**gamma
+    b = truncation(b_bar(x, y, z))**gamma
     return RGBColor(r, g, b)
-
-
-# with open("colour_conversion_test.csv", "w") as file:
-#     for l in range(250, 700):
-#         file.write(f"{l}, {r_bar(l)}, {g_bar(l)}, {b_bar(l)}\n")
